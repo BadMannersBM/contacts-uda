ab1d74d9339e69d5ae8cd873190b29de4332de57
Bruno <bruno.fdm93@gmail.com>
2020-06-27 14:33:51 -0700

    Updates

---
+++ precache-manifest.bc98c9fc70ec5f2e27247829278576e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0115b8e462e58d0c4ee8033ecf26c91c",
+    "url": "/contacts-uda/index.html"
+  },
+  {
+    "revision": "0dfa9cd9b81ed282b29d",
+    "url": "/contacts-uda/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f42f7b1d700905553da",
+    "url": "/contacts-uda/static/js/2.3b13692b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts-uda/static/js/2.3b13692b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfa9cd9b81ed282b29d",
+    "url": "/contacts-uda/static/js/main.8166afd7.chunk.js"
+  },
+  {
+    "revision": "5fd2b6e0a9d9a94eeddc",
+    "url": "/contacts-uda/static/js/runtime-main.d6e8b14e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contacts-uda/static/media/logo.5d5d9eef.svg"
+  }
+]);
